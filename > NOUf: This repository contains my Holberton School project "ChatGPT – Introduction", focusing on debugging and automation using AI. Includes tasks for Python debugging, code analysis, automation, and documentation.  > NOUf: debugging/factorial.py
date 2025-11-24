66dad053c1b0d6364c90eeefd8bc92272cd29067
Nouf Almutairi <noufn8988@gmail.com>
2025-11-24 10:00:28 +0300

    Create factorial.py

---
+++ > NOUf: This repository contains my Holberton School project "ChatGPT – Introduction", focusing on debugging and automation using AI. Includes tasks for Python debugging, code analysis, automation, and documentation.  > NOUf: debugging/factorial.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+def factorial(n):
+    if n < 0:
+        return 0 
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
