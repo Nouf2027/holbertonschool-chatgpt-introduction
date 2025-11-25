a92ae68e11933ff95256f17be39bc9df54a60529
Nouf Almutairi <noufn8988@gmail.com>
2025-11-25 11:20:41 +0300

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
-    if n < 0:
-        return 0 
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
+    """
+    Function description:
+    Computes the factorial of a given non-negative integer n using recursion.
+
+    Parameters:
+        n (int): The number to compute the factorial for.
+
+    Returns:
+        int: The factorial of n.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 f = factorial(int(sys.argv[1]))
 print(f)
