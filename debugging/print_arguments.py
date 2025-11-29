7070df6db1101d5a8be3bf27bb8abfc77bedb593
Nouf Almutairi <noufn8988@gmail.com>
2025-11-29 09:18:47 +0000

    fix print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+# Loop through the command-line arguments, skipping the script name
+for arg in sys.argv[1:]:
+    print(arg)
